9088728e30783255fd283ba0a72ab820a9a4e718
Neeraj Kumar <justneeraj2016@gmail.com>
2023-01-13 13:22:00 +0530

    initial commit

--- file.js
+++ file.js
@@ -2,20 +2,13 @@ const http = require("http");
 const fs = require("fs");
 const { type } = require("os");
 
-//for getting the current timestamp
+//for getting the files of the timestamp in a particular folder
 const server = http.createServer((req, res) => {
-  //writing file
-  let date = new Date();
-  let timeStamp = date.getTime();
-  fs.writeFileSync(`${timeStamp}.txt`, `${date}`, "utf-8");
-  let data = fs.readFileSync(`${timeStamp}.txt`, "utf-8");
+  let data = fs.readFileSync("./downloads/sample.txt", "utf-8");
   res.writeHead(200, { "Content-Type": "text/html" });
-  res.end(`${data}`);
+  res.end(data);
 });
 
 server.listen(5000, () => {
   console.log("sever is running on 5000");
 });
-
-
-  
